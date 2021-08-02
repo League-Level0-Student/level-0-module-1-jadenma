1699432cbe823f7a02f5f1b2bda42888839e77b6
student <student@ip-172-31-58-236.us-west-2.compute.internal>
2021-08-02 16:59:38 -0700

    did unbirthday

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,23 @@
+package _03_if_else._1_unbirthday;
+
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Date date = new Date();
+String currentDate = (date.getMonth() + 1) + "/" + (date.getDay()+1);
+String input = JOptionPane.showInputDialog("When is your birthday?");
+
+if (input.equalsIgnoreCase(currentDate)) {
+	JOptionPane.showMessageDialog(null, "Happy Birthday!");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Happy Unbirthday!");
+}
+	}
+
+}
