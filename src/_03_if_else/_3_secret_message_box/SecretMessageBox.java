430508b24fd2c935c45c627ac113948119c50716
student <student@ip-172-31-51-253.us-west-2.compute.internal>
2021-08-23 16:59:49 -0700

    did secret message box and tasty tomato

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,18 +17,25 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 public static void main(String[] args) {
 	
-}
+
 	// 0. Make a main method and put steps 1-5 inside it
 	
 	// 1. Set a password in a String variable
 String password = "qwertyuiopasdfghjklzxcvbnm";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
 String input = JOptionPane.showInputDialog("Type a secret message.");
+
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+String guess = JOptionPane.showInputDialog("You can only see the secret message of you know the password.");
 	// 4. If their guess matches the password, show them the secret message
-
+if (guess.equalsIgnoreCase(password)) { 
+	JOptionPane.showMessageDialog(null, input);
+}
+else { 
+	JOptionPane.showMessageDialog(null, "INTRUDER!!");
+}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
+}
 }
--- src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -2,9 +2,15 @@ void setup() {
     size(500, 500);
 }
 void draw() {
-    background(200, 200, 200);
     noStroke();
+    background(250, 250, 250);
+    fill(#FF0000);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
+    fill(#25E533);
     rect(176, 103, 12, 32);
+    fill(#FFFFFF);
+    if (mousePressed) { 
+      ellipse(75, 200, 25, 25);
+    }
 }
