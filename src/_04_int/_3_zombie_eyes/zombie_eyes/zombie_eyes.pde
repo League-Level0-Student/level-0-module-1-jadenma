af0054a247d61825d6da148543c0c54eab0ed960
student <student@ip-172-31-62-144.us-west-2.compute.internal>
2021-09-27 17:12:43 -0700

    finished zombie eyes, did rocket ship, did My Ages, and started Badgers.

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -6,5 +6,10 @@ zombie.resize(500, 500);
 }
 void draw() {
 image(zombie, 0, 0);
+fill(mouseX, mouseY, mouseY);
 ellipse(185, 220, 60, 60);
+ellipse(340, 190, 60, 60);
+fill(0);
+ellipse(185, 220, 10, 10);
+ellipse(340, 190, 10, 10);
 }
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,13 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+public static void main(String[] args) {
+	String bad = "Badger";
+	int count = 0;
+	while (count <12) { 
+		System.out.println(bad);
+		count++;
+	}
+	
+}
+}
