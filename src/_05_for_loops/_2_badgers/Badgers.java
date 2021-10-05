7bca9e18dab0cb1ea8d6b3387905e467494c91a1
student <student@ip-172-31-48-135.us-west-2.compute.internal>
2021-10-04 17:01:16 -0700

    finished badgers song and did bananas and read banana split intructions.

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -5,9 +5,41 @@ public static void main(String[] args) {
 	String bad = "Badger";
 	int count = 0;
 	while (count <12) { 
-		System.out.println(bad);
+		System.out.print(bad);
+		if (count <11)
+			System.out.print(", ");
 		count++;
 	}
+	System.out.println();
+	String Mush = "Mushroom";
+	int secondCount = 0;
+	while (secondCount <2) { 
+		System.out.print(Mush);
+		if (secondCount <1)
+			System.out.print(", ");
+		secondCount++;
+	}
+	System.out.println();
+	System.out.println();
+	int thirdCount = 0;
+	while (thirdCount <12) {
+		System.out.print(bad);
+		if (thirdCount <11)
+			System.out.print(", ");
+		thirdCount++;
+	}
+	System.out.println();
+	int fourthCount = 0;
+	while (fourthCount <2) { 
+		System.out.print(Mush);
+		if (fourthCount <1) 
+			System.out.print(", ");
+		fourthCount++;
+	}
+	System.out.println();
+	System.out.println();
+	String sna = "A Snake!!!";
+	System.out.println(sna);
 	
 }
 }
---
+++ src/_05_for_loops/_3_bananas_4eva/bananas_4eva/bananas_4eva.pde
@@ -0,0 +1,4 @@
+int count=0;
+for ( count=0; count <= 1000; count++) {
+  System.out.print("banana ");
+}
